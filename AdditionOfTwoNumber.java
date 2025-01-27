345a81494fb37bbbb4fd043856dabd433b4e686f
Neha Sapkal <sapkalneha90@gmail.com>
2025-01-27 23:29:26 +0530

    Add files via upload

---
+++ AdditionOfTwoNumber.java
@@ -0,0 +1,10 @@
+package BasicOfJava;
+
+public class AdditionOfTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AreaOfSquare.java
@@ -0,0 +1,14 @@
+package BasicOfJava;
+
+public class AreaOfSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//CALCULATE AREA OF SQUARE
+		int side = 20;
+		float area = side*side;
+		System.out.println("Area of Square : "+area);
+
+	}
+
+}
